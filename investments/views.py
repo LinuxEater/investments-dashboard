9c6d65c64b3e7d0113a3e8f746e836ff9dc5ef62
LinuxEater <moisessouzasantos001@gmail.com>
2025-07-03 15:30:34 -0300

    Create the dashboard view

--- investments/views.py
+++ investments/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Investment
 
 # Create your views here.
+
+def dashboard_view(request):
+    investments = Investment.objects.all()
+    total = sum([inv.amount_invested for inv in investments])
+    return render(request, 'dashboard.html', {
+        'investments': investments,
+        'total': total
+    })
